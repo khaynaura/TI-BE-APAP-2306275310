e30d822e32b3cffd7e6915926a07fbfb3cbc289d
khaynaura <grankhay@gmail.com>
2025-11-02 20:32:42 +0700

    Initial commit

---
+++ src/main/java/apap/ti/_5/Insurance_2306275310_be/Insurance2306275310BeApplication.java
@@ -0,0 +1,13 @@
+package apap.ti._5.Insurance_2306275310_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Insurance2306275310BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Insurance2306275310BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apap/ti/_5/Insurance_2306275310_be/Insurance2306275310BeApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti._5.Insurance_2306275310_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Insurance2306275310BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
