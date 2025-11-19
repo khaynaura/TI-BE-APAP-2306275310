42dcb00544c0bb1a822c554a6efce9bf8c8e860f
khaynaura <grankhay@gmail.com>
2025-11-19 12:40:54 +0700

    tutorial 9

--- src/main/java/apap/ti/_5/Insurance_2306275310_be/config/CorsConfig.java
+++ src/main/java/apap/ti/_5/Insurance_2306275310_be/config/CorsConfig.java
@@ -4,19 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry
-                        .addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173"
-                        )
+                String[] origins = allowedOrigins.split(",");
+
+                registry.addMapping("/**")
+                        .allowedOrigins(origins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
