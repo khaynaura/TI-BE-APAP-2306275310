6ed6a97327ac6914ed790b718ee5180ba01e0df4
khaynaura <grankhay@gmail.com>
2025-11-19 18:57:01 +0700

    smoga yaAllah

--- src/main/java/apap/ti/_5/Insurance_2306275310_be/config/CorsConfig.java
+++ src/main/java/apap/ti/_5/Insurance_2306275310_be/config/CorsConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+//fixed
+
 @Configuration
 public class CorsConfig {
     @Value("${CORS_ALLOWED_ORIGINS}")
